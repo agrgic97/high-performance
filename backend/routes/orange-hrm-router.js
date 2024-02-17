9a10c57a2d4a5b7254d6ceb7a80e2b30721176a5
agrgic2s <antonio.grgic@smail.inf.h-brs.de>
2024-02-17 21:34:51 +0100

    deleted unused routes and controllers

--- backend/routes/orange-hrm-router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const orangeHRMController = require("../controller/orange-hrm-controller")
-
-router.get("/salesmen", orangeHRMController.getAllSalesmen)
-router.get("/salesman/:id", orangeHRMController.getSalesmanById)
-router.get("/employees", orangeHRMController.getAllEmployees)
-
-module.exports = router
